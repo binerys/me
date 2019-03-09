461c8648123feb146ae626337cf3eac55266ea1f
binerys <bree.nery@gmail.com>
2019-03-08 20:48:05 -0800

    Another attempt at fixing window issue

--- src/components/Swatch/Hex/Hex.js
+++ src/components/Swatch/Hex/Hex.js
@@ -4,20 +4,29 @@ import Clipboard from 'react-clipboard.js'
 import styles from './styles.module.css'
 import { Value } from './styles';
 
-const Hex = props => (
-  <>
-    <Value data-tip="Copied!" data-event="click focus">
-      <Clipboard component="a" data-clipboard-text={props.value}>
-        {props.value}
-      </Clipboard>
-    </Value>
-    <ReactTooltip
-      className={styles.tooltip}
-      effect="solid"
-      globalEventOff="click"
-      place="right"
-    />
-  </>
-)
+const Hex = props => {
+  // Workaround for window not defined error
+  if (typeof window !== 'undefined') {
+    return (
+      <>
+        <Value data-tip="Copied!" data-event="click focus">
+          <Clipboard component="a" data-clipboard-text={props.value}>
+            {props.value}
+          </Clipboard>
+        </Value>
+        <ReactTooltip
+          className={styles.tooltip}
+          effect="solid"
+          globalEventOff="click"
+          place="right"
+        />
+      </>
+    );
+  } else {
+    return (
+      <> props.value </>
+    );
+  }
+};
 
 export default Hex;
